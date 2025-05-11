298a90cdf3099714ef6483f405d22bb770b2b245
DenisVas <levithmasta@gmail.com>
2025-05-12 03:51:00 +0400

    new tests

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    WebDriver browser;
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        //options.addArguments("headless");
+        browser = new ChromeDriver(options);
+        browser.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void close(){
+        browser.quit();
+    }
+}
